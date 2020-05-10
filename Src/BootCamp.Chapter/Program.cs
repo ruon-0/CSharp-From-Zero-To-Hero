93a4ef63bee12183bd91ff848db70faea8944d57
Ruon-0 <mail.onurdeniz@gmail.com>
2020-05-10 11:48:59 +0200

    Homework done

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,48 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = "";
+            var surename = "";
+            var age = "";
+            var weight = "";
+            var height = "";
+
+            Console.WriteLine("Hello World! This is my first homework assignment. Let's go!");
+            Console.WriteLine("\nPlease put the following information in:");
+            //Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm.
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+            Console.Write("Surname: ");
+            surename = Console.ReadLine();
+            Console.Write("Age: ");
+            age = Console.ReadLine();
+            Console.Write("Weight in kg: ");
+            weight = Console.ReadLine();
+            Console.Write("Height in cm: ");
+            height = Console.ReadLine();
+
+            Console.WriteLine("\nWell done. Let's see who we got");
+            Console.WriteLine(name + " " + surename + " is " + age + " years old, his weight is " + weight
+                + " kg and his height is " + height + " cm.");
+
+            Console.WriteLine("\nThat was fun, let's do another one!");
+            Console.WriteLine("\nPlease put the following information in:");
+
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+            Console.Write("Surname: ");
+            surename = Console.ReadLine();
+            Console.Write("Age: ");
+            age = Console.ReadLine();
+            Console.Write("Weight in kg: ");
+            weight = Console.ReadLine();
+            Console.Write("Height in cm: ");
+            height = Console.ReadLine();
+
+            Console.WriteLine("\nWell done. Let's see who we got");
+            Console.WriteLine("\n" + name + " " + surename + " is " + age + " years old, his weight is " + weight
+                + " kg and his height is " + height + " cm.");
+
         }
     }
 }
